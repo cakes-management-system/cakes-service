0b191ec4f15ad720a467c5ca17a7709eea6125db
Alexander Koltsov <alexander.koltsov.work@gmail.com>
2022-02-09 19:38:35 +0300

    Enable feign client

--- src/main/java/com/koltsov/cms/service/cakes/CakesServiceApplication.java
+++ src/main/java/com/koltsov/cms/service/cakes/CakesServiceApplication.java
@@ -2,7 +2,9 @@ package com.koltsov.cms.service.cakes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CakesServiceApplication {
 
