44934d6995fd4ae20134197b8c7ff2568ac50a89
Alexander Koltsov <alexander.koltsov.work@gmail.com>
2022-02-09 15:18:53 +0300

    CI added

--- src/main/java/com/koltsov/cms/service/cakes/CakeController.java
+++ src/main/java/com/koltsov/cms/service/cakes/CakeController.java
@@ -1,7 +1,5 @@
 package com.koltsov.cms.service.cakes;
 
-import com.koltsov.cms.starter.mapper.GenericMapper;
-import com.koltsov.cms.starter.service.CrudService;
 import com.koltsov.cms.starter.web.controller.CrudController;
 import com.koltsov.cms.starter.web.dto.cake.CakeCreateDto;
 import com.koltsov.cms.starter.web.dto.cake.CakeDto;
@@ -11,9 +9,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/cakes")
 public class CakeController extends CrudController<Cake, Long, CakeDto, CakeCreateDto> {
-
-    public CakeController(CrudService<Cake, Long> crudService,
-                          GenericMapper<Cake, CakeDto, CakeCreateDto> mapper) {
-        super(crudService, mapper);
-    }
 }
--- src/main/java/com/koltsov/cms/service/cakes/CakeService.java
+++ src/main/java/com/koltsov/cms/service/cakes/CakeService.java
@@ -1,15 +1,10 @@
 package com.koltsov.cms.service.cakes;
 
 import com.koltsov.cms.starter.service.DefaultCrudService;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CakeService extends DefaultCrudService<Cake, Long> {
-
-    public CakeService(JpaRepository<Cake, Long> repository) {
-        super(repository);
-    }
 
     @Override
     protected void updateFields(Cake to, Cake from) {
